d1a6d2863c70fee09b8c6b1e8558aacda5e170de
uwec <uwe650@hotmail.com>
2014-08-16 19:25:17 -0400

    adding decentralwallet.com website repo

---
+++ js/init.js
@@ -0,0 +1,70 @@
+$(window).load(function () {
+	"use strict";
+	$('#status').fadeOut();
+	$('#preloader').delay(350).fadeOut('slow');
+	$('body').delay(350).css({
+		'overflow': 'visible'
+	});
+});
+$(function () {
+	"use strict";
+
+	/* ---------------------------------------------------------
+	 * Background (Backstretch)
+	 */
+
+	$.backstretch([
+		"img/background/1.jpg",
+		"img/background/2.jpg",
+		"img/background/3.jpg"
+	], {duration: 3800, fade: 1500});
+	
+	/* ---------------------------------------------------------
+	 * WOW
+	 */
+	
+	new WOW().init();
+	
+	/* ---------------------------------------------------------
+	 * Scroll arrow
+	 */
+	
+	$("#scroll").click(function () {
+	 	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	 		var target = $(this.hash);
+	 		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	 		if (target.length) {
+	 			$('html,body').animate({
+	 				scrollTop: target.offset().top
+	 			}, 1200);
+	 			return false;
+	 		}
+	 	}
+	 });
+
+	/* ---------------------------------------------------------
+	 * Countdown
+	 */
+
+	var description = {
+		weeks: "weeks",
+		days: "days",
+		hours: "hours",
+		minutes: "minutes",
+		seconds: "seconds"
+	};
+	
+	// year/month/day
+	$('#countdown').countdown('2014/9/15', function (event) {
+		$(this).html(event.strftime(
+			'<div class="countdown-section"><b>%w</b> <span>' + description.weeks + '</span> </div>' +
+			'<div class="countdown-section"><b>%d</b> <span>' + description.days + '</span> </div>' +
+			'<div class="countdown-section"><b>%H</b> <span>' + description.hours + '</span> </div>' +
+			'<div class="countdown-section"><b>%M</b> <span>' + description.minutes + '</span> </div>' +
+			'<div class="countdown-section"><b>%S</b> <span>' + description.seconds + '</span> </div>'
+		));
+	});
+
+
+
+});
